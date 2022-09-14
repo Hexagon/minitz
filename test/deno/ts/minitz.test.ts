fa38004dd7f5ce9d69097666042edc95212f8456
Hexagon <hexagon@56k.guru>
2022-09-15 00:10:24 +0200

    Fix Deno test

--- test/deno/ts/minitz.test.ts
+++ test/deno/ts/minitz.test.ts
@@ -1,64 +1,42 @@
-import { assertEquals, assertNotEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.128.0/testing/asserts.ts";
 import { minitz } from "../../../src/minitz.js";
+import type { TimePoint } from "../../../src/minitz.js";
 
 Deno.test("Convert to different timezone and back", function () {
   const sourceUTCDate = new Date(Date.UTC(2022, 0, 1, 0, 0, 0)),
-    dateInNewYork = minitz.toTZ(sourceUTCDate, "America/New_York"),
-    backToUTC = minitz.fromTZ(dateInNewYork, "America/New_York");
+    dateInNewYork = minitz.toTZ(sourceUTCDate, "America/New_York");
+
+  const backToUTC = minitz.fromTZ(dateInNewYork);
 
   // Source date equal
   assertEquals(sourceUTCDate.getTime(), backToUTC.getTime());
-
-  // Source date does not equal date in santiago
-  assertNotEquals(sourceUTCDate.getTime(), dateInNewYork.getTime());
 });
 
 Deno.test("Test DST transition", function () {
-  const localVsRemote = function (
-    localTZ: string,
-    local: string,
-    remoteTZ: string,
-    remote: string,
-  ) {
-    const timeInLocal = minitz.fromTZ(new Date(Date.parse(local)), localTZ),
-      timeInRemote = minitz.fromTZ(new Date(Date.parse(remote)), remoteTZ);
+  const localVsRemote = function (local: TimePoint, remote: TimePoint) {
+    const timeInLocal = minitz.fromTZ(local),
+      timeInRemote = minitz.fromTZ(remote);
     assertEquals(timeInLocal.getTime(), timeInRemote.getTime());
   };
 
   localVsRemote(
-    "Europe/Stockholm",
-    "2022-03-13 06:08:09",
-    "America/New_York",
-    "2022-03-13 00:08:09",
+    minitz.tp(2022, 3, 13, 6, 8, 9, "Europe/Stockholm"),
+    minitz.tp(2022, 3, 13, 0, 8, 9, "America/New_York"),
   );
   localVsRemote(
-    "Europe/Stockholm",
-    "2022-03-13 07:08:09",
-    "America/New_York",
-    "2022-03-13 01:08:09",
+    minitz.tp(2022, 3, 13, 7, 8, 9, "Europe/Stockholm"),
+    minitz.tp(2022, 3, 13, 1, 8, 9, "America/New_York"),
   );
   localVsRemote(
-    "Europe/Stockholm",
-    "2022-03-13 08:08:09",
-    "America/New_York",
-    "2022-03-13 02:08:09",
+    minitz.tp(2022, 3, 13, 8, 8, 9, "Europe/Stockholm"),
+    minitz.tp(2022, 3, 13, 3, 8, 9, "America/New_York"),
   ); // <-- New york DST transition 02:00 -> 03:00
   localVsRemote(
-    "Europe/Stockholm",
-    "2022-03-13 08:08:09",
-    "America/New_York",
-    "2022-03-13 03:08:09",
-  ); // <-- New york DST transition 02:00 -> 03:00
-  localVsRemote(
-    "Europe/Stockholm",
-    "2022-03-13 09:08:09",
-    "America/New_York",
-    "2022-03-13 04:08:09",
+    minitz.tp(2022, 3, 13, 9, 8, 9, "Europe/Stockholm"),
+    minitz.tp(2022, 3, 13, 4, 8, 9, "America/New_York"),
   );
   localVsRemote(
-    "Europe/Stockholm",
-    "2022-03-13 10:08:09",
-    "America/New_York",
-    "2022-03-13 05:08:09",
+    minitz.tp(2022, 3, 13, 10, 8, 9, "Europe/Stockholm"),
+    minitz.tp(2022, 3, 13, 5, 8, 9, "America/New_York"),
   );
 });
