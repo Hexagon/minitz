89fbf7ab1f92466c966eb13afbf830a4245777b5
Hexagon <hexagon@56k.guru>
2023-12-03 19:18:54 +0000

    Fix bug making dates with AKDT offsets result in Invalid Date. Release 4.0.6.

--- dist/minitz.cjs
+++ dist/minitz.cjs
@@ -234,7 +234,7 @@
 	function getTimezoneOffset(timeZone, date = new Date()) {
 
 		// Get timezone 
-		const tz = date.toLocaleString("en-US", {timeZone: timeZone, timeZoneName: "short"}).split(" ").slice(-1)[0];
+		const tz = date.toLocaleString("en-US", {timeZone: timeZone, timeZoneName: "shortOffset"}).split(" ").slice(-1)[0];
 
 		// Extract time in en-US format
 		// - replace narrow no break space with regular space to compensate for bug in Node.js 19.1
